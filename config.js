74e57a49e60022dcb728b5d355a54334823b51c1
mmyeuem <88323324+mmyeuem@users.noreply.github.com>
2021-08-02 18:30:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,15 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'Ấn vào đây nè :3',
+    title: 'Bức ảnh lúc nãy đã làm em mỉm cười trong chốc lát',
+    desc: 'nhưng anh có thể làm em mỉm cười cả cuộc đời được không ',
+    btnYes: 'Được chứ , vì em yêu anh <33',
+    btnNo: 'Không, anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà . Yêu em lắm lun ó 😘😘',
+    messDesc: 'Hết dịch gặp nhau nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/messages/t/100057277666022' 
